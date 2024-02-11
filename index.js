9a0be3e006b0c642d2682efb1e80a53af0a92e36
tjay1760 <mcjthiongo@gmail.com>
2024-02-11 16:06:35 +0300

    Succesfull get

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const Joi = require('joi');
 const mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost/classroom")
+.then(()=>console.log("Connected to classroomDB sucessfully"))
+.catch(e=>console.log(e))
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-app.listen(5000,()=>{
+app.listen(port,()=>{
     console.log(`listening on port ${port}`)
 })
-app.get('/',(req,res)=>{
-res.send('home')
+const classroomSchema = mongoose.Schema({
+    isGold: Boolean,
+    name: String,
+    phone:String
+})
+
+Classroom = mongoose.model("Classroom", classroomSchema)
+
+app.get('/',async (req,res)=>{
+    const classrooms = await Classroom.find()  
+res.send(classrooms)
 })
 app.post('/',(req,res)=>{
 
